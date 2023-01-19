fb1871cfde076b8f04e5d91b93a8420eae01240f
tohhuishan <tohhuishan12@gmail.com>
2023-01-19 21:41:24 +0800

    Initial commit

---
+++ source/script.js
@@ -0,0 +1,12 @@
+function showDate(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+let selectElement = document.querySelector("#city");
+selectElement.addEventListener("change", showDate);
